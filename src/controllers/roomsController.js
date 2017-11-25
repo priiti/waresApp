d206618456287cf8a028acfd593a4f00d03184c3
martti <mrt@mrt.ee>
2017-11-25 22:05:11 +0200

    Add roomController

---
+++ src/controllers/roomsController.js
@@ -0,0 +1,41 @@
+const Room = require('./../models/Room');
+
+exports.getRooms = async (req, res) => {
+  try {
+    const rooms = await Room.find({});
+    res.send(rooms);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.getRoom = async (req, res) => {
+  try {
+    const { roomId } = req.params;
+    const room = await Room.findById(roomId);
+    res.send(room);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.createRoom = async (req, res) => {
+  try {
+    const { name, description } = req.body;
+    if (!name || !description) {
+      throw new Error('Name or description is missing!');
+    }
+
+    const room = await Room.findOne({ name });
+    if (room) {
+      throw new Error(`Room ${name} already exists!`);
+    }
+
+    const newRoom = new Room({ name, description });
+    await newRoom.save();
+    res.status(200).send(newRoom);
+  } catch (error) {
+    console.error(error);
+    res.status(400).send(error.message);
+  }
+};
