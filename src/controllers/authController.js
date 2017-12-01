dd2818c339c38b43301a6e6469270a172224f817
Priit Pärl <priitparl@gmail.com>
2017-12-01 14:50:28 +0200

    auth controller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,37 @@
-exports.registerUser = async (req, res, next) => {
+const User = require('./../models/User');
+const crypto = require('crypto');
 
+const { Error } = require('./../utils/errorHandlers');
+
+exports.registerUser = async (req, res, next) => {
+  try {
+    const {
+      firstName, lastName, phoneNumber, email, password
+    } = req.body;
+
+    const existingUser = await User.findOne({ 'login.email': email });
+    if (existingUser) {
+      throw new Error(`Account with email address ${email} already exists.`);
+    }
+
+    const user = await new User({
+      firstName,
+      lastName,
+      phoneNumber,
+      login: {
+        email,
+        password
+      }
+    }).save();
+
+    if (!user) {
+      throw new Error('User was not created!');
+    }
+
+    return res.status(201).json({ message: 'User was successfully created.' });
+  } catch (err) {
+    next(err);
+  }
 };
 
 exports.login = async (req, res, next) => {
@@ -7,13 +39,65 @@ exports.login = async (req, res, next) => {
 };
 
 exports.forgotPassword = async (req, res, next) => {
+  try {
+    const resetLinkSentSuccessMessage = `Password reset link has been sent to ${email}.`;
 
+    const user = await User.findOne({ 'login.email': req.body.email });
+    if (!user) {
+      return res.json({ message: resetLinkSentSuccessMessage });
+    }
+
+    const { email, passwordResetExpires } = user.login;
+
+    const passwordResetSentTimestamp =
+      passwordResetExpires - parseInt(process.env.PASSWORD_RESET_EXPIRES, 10) > Date.now();
+
+    if (passwordResetSentTimestamp &&
+        passwordResetSentTimestamp +
+        parseInt(process.env.PASSWORD_RESET_LINK_LIMIT, 10) > Date.now()) {
+      throw new Error('Password reset link has been already sent. Please wait to request new one.');
+    }
+
+    user.login = {
+      ...user.login,
+      passwordResetToken: crypto.randomBytes(20).toString('hex'),
+      passwordResetExpires: Date.now() + parseInt(process.env.PASSWORD_RESET_EXPIRES, 10)
+    };
+
+    await user.save();
+
+    // const passwordResetUrl =
+    //   `${process.env.SITE_URL}/account/password/${user.login.passwordResetToken}`;
+
+    // TODO: implement mail sending
+
+    res.json({ message: resetLinkSentSuccessMessage });
+  } catch (err) {
+    next(err);
+  }
 };
 
-exports.resetPassword = async (req, res, next) => {
+exports.validatePasswordResetToken = async (req, res, next) => {
+  try {
+    const user = await User.findOne({
+      'login.passwordResetToken': req.params.token,
+      'login.passwordResetExpires': { $gt: Date.now() }
+    });
 
+    if (!user) {
+      throw new Error('Password reset link invalid or expired. Please try again or contact your administrator');
+    }
+
+    res.json({ message: 'Please fill required fields for password reset.' });
+  } catch (err) {
+    next(err);
+  }
 };
 
 exports.updatePassword = async (req, res, next) => {
+  try {
 
+  } catch (err) {
+    next(err);
+  }
 };
