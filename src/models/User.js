7bcc9e9706090bc2dae69f7c0476e99694411743
Priit Pärl <priitparl@gmail.com>
2017-12-02 13:17:23 +0200

    Auth controller (#22)
    * Auth controller

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
@@ -7,7 +8,10 @@ const userSchema = new mongoose.Schema({
   phoneNumber: { type: String, required: true },
   login: {
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    passwordResetToken: String,
+    passwordResetExpires: Date,
+    passwordUpdatedAt: { type: Date, default: null }
   },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date },
@@ -15,6 +19,33 @@ const userSchema = new mongoose.Schema({
 }, {
   collection: 'users'
 });
+
+/**
+ * Salt and hash password before saving the user
+ */
+userSchema.pre('save', async function (next) {
+  try {
+    const user = this;
+    if (!user.isModified('login.password')) {
+      return next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(user.login.password, salt);
+    user.login.password = hash;
+
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/**
+ * Validate user given password with salted/hashed password
+ */
+userSchema.methods.validatePasswords = function (userGivenPassword) {
+  return bcrypt.compare(userGivenPassword, this.login.password);
+};
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
