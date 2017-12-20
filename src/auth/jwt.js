aaf11bae01ef3e6fda623bf9e93cac897385f5d3
Priit Pärl <priitparl@gmail.com>
2017-12-21 00:19:00 +0200

    Fix typo

--- src/auth/jwt.js
+++ src/auth/jwt.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 
 const jwtSecret = process.env.SECRET;
 const validateJwt = expressJwt({ secret: jwtSecret });
-const logger = require('./logger');
+const logger = require('./../utils/logger');
 const Token = require('./../models/Token');
 const User = require('./../models/User');
 const { NotAuthorizedError, ServerError } = require('./../utils/errorHandlers');
